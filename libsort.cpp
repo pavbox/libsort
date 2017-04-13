46289ab8afc30ec897fa20537fe0f7a1046d8d69
pavbox <pavboxdev@gmail.com>
2017-04-13 21:43:41 +0300

    add selection and bubble sorts

--- libsort.cpp
+++ libsort.cpp
@@ -20,74 +20,137 @@
  */
 namespace Libsort {
 
-
   void print() {
     std::cout << "/* message */" << '\n';
   }
 
-
   void generateArray(int* array) {
     srand(time(0));
-    for (int i = START_ARRAY; i < LENGTH_ARRAY; i++) {
-      array[i] = rand() % 100000 + 1; // 1..100
+    for (int idx_i = 1; idx_i < 20000; idx_i++) {
+      array[idx_i] = rand() % 100000 + 1; // 1..100
     }
   }
-
 
   void copyArray(int* original, int* clone) {
-    for (int i = START_ARRAY; i < LENGTH_ARRAY; i++) {
-      clone[i] = original[i]; // copy array
+    for (int idx_i = 1; idx_i < 20000; idx_i++) {
+      clone[idx_i] = original[idx_i]; // copy array
     }
   }
 
-
-
   void outputArray(int* array) {
-    for (int i = START_ARRAY; i < LENGTH_ARRAY; i++) {
-      std::cout << array[i] << ' '; // copy array
-      if (i % 15 == 0) {
+    for (int idx_i = 1; idx_i < 20000; idx_i++) {
+      std::cout << array[idx_i] << ' '; // copy array
+      if (idx_i % 15 == 0) {
         // cout << endl;
       }
     }
   }
 
 
-  void insertSort(int* array) {
-    int j = 0;
+
+  void insertion(int* array, int size) {
+    int idx_finder = 0;
     double value;
 
-    for (int i = START_ARRAY + 1; i < LENGTH_ARRAY; i++) {
-      value = array[i];
+    for (int idx_i = 2; idx_i < size; idx_i++) {
+      value = array[idx_i];
       array[0] = value; // barier
-      j = i;
+      idx_finder = idx_i;
 
-      while (value < array[j - 1]) {
-        array[j] = array[j - 1];
-        j--;
+      while (value < array[idx_finder - 1]) {
+        array[idx_finder] = array[idx_finder - 1];
+        idx_finder--;
       }
-      array[j] = value;
+      array[idx_finder] = value;
     }
   }
 
 
 
-  void quickSort(int* array, int first, int last) {
-      int i = first, j = last, value = array[(first + last) / 2];
+  void insertionBisection(int* array, int size) {
+    int idx_i = 0;
+    int idx_j = 0;
+    int idx_mid = 0;
+    int idx_left = 0;
+    int idx_right = 0;
+
+    int target = 0;
+    double value;
+
+    for (idx_i = 2; idx_i < size; idx_i++) {
+      value = array[i];
+      idx_left = 1;
+      idx_right = i;
+
+      while (idx_left < idx_right) {
+        idx_mid = (first + last) / 2;
+        target = array[idx_mid];
+
+        if (target < value) {
+          idx_left = idx_mid + 1;
+        } else {
+          idx_right = idx_mid;
+        }
+      }
+
+      for (idx_j = (idx_right + 1); idx_j <= idx_i; idx_j--) {
+        array[idx_j] = array[idx_j - 1];
+      }
+
+      array[idx_right] = value;
+    }
+  }
+
+
+
+
+  void selection(int* array, int size) {
+      for (int idx_i = 0; idx_i < size - 1; idx_i++) {
+          int idx_min = idx_i;
+          for (int j = idx_i + 1; j < size; j++) {
+              if (array[j] < array[idx_min]){
+                  idx_min = j;
+              }
+          }
+
+          if (idx_min != idx_i){
+              std::swap(array[idx_i], array[idx_min]);
+          }
+      }
+  }
+
+
+
+  void bubble(int* array, int size) {
+    double value;
+
+    for (int idx_i = 2; idx_i < size; idx_i++) {
+      for (int idx_j = size; idx_j <= idx_i; idx_j++) {
+        std::swap(array[idx_j], array[idx_j - 1]);
+      }
+    }
+  }
+
+
+
+  void quick(int* array, int first, int last) {
+      int idx_i = first, idx_j = last, value = array[(first + last) / 2];
 
       do {
-          while (array[i] < value) i++;
-          while (array[j] > value) j--;
+          while (array[idx_i] < value) idx_i++;
+          while (array[idx_j] > value) idx_j--;
 
-          if (i <= j) {
-              if (array[i] > array[j]) std::swap(array[i], array[j]);
-              i++;
-              j--;
+          if (idx_i <= idx_j) {
+              if (array[idx_i] > array[idx_j]) std::swap(array[idx_i], array[idx_j]);
+              idx_i++; idx_j--;
           }
-      } while (i <= j);
+      } while (idx_i <= idx_j);
 
-      if (i < last)
-          quickSort(array, i, last);
-      if (first < j)
-          quickSort(array, first, j);
+      if (last  > idx_i) quick(array, idx_i, last);
+      if (first < idx_j) quick(array, first, idx_j);
   }
+
+
+
+
 }
--- libsort.h
+++ libsort.h
@@ -1,6 +1,13 @@
 
 /**
 
+ 1. insertion sort (+bisection)
+ 2. selection sort
+ 3. bubble sort
+ 4. shaker sort
+ 5. shells sort
+ 6. quick sort (+non-recursive)
+
 
 */
 
@@ -9,14 +16,20 @@
  */
 
 namespace Libsort {
-  static const int LENGTH_ARRAY = 20000;
-  static const int START_ARRAY = 1; // from 1 to 150k
+  // static const int LENGTH_ARRAY = 20000;
+  // static const int START_ARRAY = 1; // from 1 to 20k
+
+  // TODO: CHECK ALL ALGS
 
   void print();
   void generateArray(int* array);
   void copyArray(int* original, int* clone);
-
   void outputArray(int* array);
-  void insertSort(int* array);
-  void quickSort(int* array, int first, int last);
+
+  void insertion(int* array, int size);
+  void insertionBisection(int* array, int size);
+  void selection(int* array, int size); // need check code
+  void quick(int* array, int first, int last);
+
+  void bubble(int* array, int size);
 }
